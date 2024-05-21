dae24717b3004156e8e086c9917bb3498f2191d2
Aejaz Ahmed <mdaejazahmed0123@gmail.com>
2024-05-21 19:13:25 +0530

    bd connection

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const job_routers = require("./mvc/router/job_router") // import router
 
 const app = express();
+//mongodb://localhost:27017/job_application
 
-mongoose.connect("mongodb://localhost:27017/job_application")
+mongoose.connect("mongodb+srv://mdaejazahmed6692:Aejaz123@jobapp.qpkj84e.mongodb.net/")
 .then(()=>{
     console.log("project is connected with data base");
 })
